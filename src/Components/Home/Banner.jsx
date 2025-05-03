109c4a678e0e024d8cb0c71ea27732fed643625c
krunal-ar-panhal <krunalapnchalkp2123@gmail.com>
2025-05-03 09:56:03 +0530

    art

--- src/Components/Home/Banner.jsx
+++ src/Components/Home/Banner.jsx
@@ -100,7 +100,7 @@ const Banner = () => {
   
 
   return (
-    <Slider {...settings}>
+    <Slider {...settings} className="banner-slider w-full h-[50vh] sm:h-[60vh] md:h-[70vh] min-h-[350px] font-marcellus">
       <AnimatePresence>
         <motion.div
           className="relative w-full h-[50vh] sm:h-[60vh] md:h-[70vh] min-h-[350px]"
--- src/Components/Sigup.jsx
+++ src/Components/Sigup.jsx
@@ -96,40 +96,9 @@ const Signup = () => {
                     />
                   </div> */}
                <div className="flex items-center justify-center xl:justify-start lg:justify-start ">
-  <motion.button
-    whileHover={{ scale: 1.05 }}
-    whileTap={{ scale: 0.95 }}
-    className="button text-center items-center justify-center flex w-36 font-medium hover:border bg-burntCopper hover:bg-black"
-    style={{ "--clr": "#AA7446" }}
-  >
-    <span className="button__icon-wrapper">
-      <svg
-        viewBox="0 0 14 15"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        className="button__icon-svg"
-        width="10"
-      >
-        <path
-          d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
-          fill="currentColor"
-        ></path>
-      </svg>
-      <svg
-        viewBox="0 0 14 15"
-        fill="none"
-        width="10"
-        xmlns="http://www.w3.org/2000/svg"
-        className="button__icon-svg button__icon-svg--copy"
-      >
-        <path
-          d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
-          fill="currentColor"
-        ></path>
-      </svg>
-    </span>
-    Sign Up
-  </motion.button>
+               <button className="bg-burntCopper w-full hover:bg-black transition-all duration-500 cursor-pointer text-white p-3 rounded-full text-xs sm:text-sm md:text-sm  hover:bg-opacity-90">
+              Sign up
+            </button>
 </div>
 
                 </form>
